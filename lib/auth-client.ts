214742d47c6054d0bb65e1dfe4470a6290a1b5f2
pGamelax <pedrogamela2003@gmail.com>
2026-01-05 20:51:38 -0300

    alter auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://api.clashdata.pro",
+  baseURL: "https://clashdata.pro",
   fetchOptions: {
     credentials: "include",
   },
